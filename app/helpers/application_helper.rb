22d8b5e9337a19e435d01c534aebccbb630561ec
Yuxing Jiang <yuxing.jiang@gmail.com>
2019-05-28 19:06:10 +0000

    Revert "introduced app/presenters"
    This reverts commit 3ecc37d7098606bc75aff6ed338ee7168f8212df.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,18 +2,9 @@ module ApplicationHelper
 
     # a customized out link function, can add prefix to the weblink
     def link_to_external_lookup(options={})
-        url_prefix = 'http://www.amazon.com/gp/search/ref=sr_adv_b/?field-isbn='
-        options[:value].map do |isbn|
-            link_to("#{isbn}", "#{url_prefix}#{isbn}") << ' (Amazon)'
+        url_prefix = 'https://www.google.com/search?q='
+        options[:value].map do |url_fulltext_ssm|
+            link_to("#{url_fulltext_ssm}", "#{url_prefix}#{isbn}") << ''
         end
-    end
-
-    #rend local png images
-    def render_thumbnail(document, options)
-        return unless document[:file_id].present?
-        image_tag(
-          "#{image_server}/#{document.id}/#{document.first(:file_id)}.png",
-          options.merge(alt: presenter(document).document_heading)
-        )
     end
 end
