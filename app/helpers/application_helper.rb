341af1b72c1d9cdea224015e35d7593dbaf6643c
Yuxing Jiang <yuxing.jiang@gmail.com>
2019-06-05 17:23:24 +0000

    get to know the fields that not rendered just because the name of the fields are not correct. And fields name can be searched though the velocity view using the parameters from rails console

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
     # a customized out link function, can add prefix to the weblink
     def link_to_external_lookup(options={})
-        url_prefix = 'https://www.google.com/search?q='
+        #url_prefix = 'https://www.google.com/search?q='
         options[:value].map do |url_fulltext_ssm|
-            link_to("#{url_fulltext_ssm}", "#{url_prefix}#{isbn}") << ''
+            link_to("#{url_fulltext_ssm}", "#{url_fulltext_ssm}") 
         end
     end
 end
